78f2f46baab81df3e299a2b21dc71db3733d0743
sirfcsn <sirfcsn@gmail.com>
2019-01-07 23:29:12 +0900

    feat: Add initializing feat in lotto.js

--- baseball/baseball.js
+++ baseball/baseball.js
@@ -1,8 +1,8 @@
 let body = document.body;
 
 let materialNum;
-
 let numArr;
+
 function generateNum () {
     materialNum = [1, 2, 3, 4, 5, 6, 7, 8, 9];
     numArr = [];
@@ -17,7 +17,7 @@ console.log(numArr);
 
 let explain = document.createElement('p');
 document.body.append(explain);
-explain.textContent = '네자리 숫자를 입력하세요.'
+explain.textContent = 'Enter a 4-digit number.'
 let result = document.createElement('h1');
 body.append(result);
 let form = document.createElement('form');
@@ -31,10 +31,21 @@ button.textContent = '입력';
 form.append(button);
 
 let wrong = 0;
+let beforeNum = ['-', '-', '-', '-'];
+
+let ol = document.getElementById('order');
+for (const el of beforeNum) {
+    let li = document.createElement('li');
+    li.innerText = el
+    ol.appendChild(li);
+}
 
 const asyncFunc = event => {
     event.preventDefault();
     let answer = input.value;
+    if (answer.length !== 4) {
+        return result.textContent = 'You must input 4-digit Number.'
+    }
     console.log(answer, numArr, answer === numArr.join(''));
     if (answer === numArr.join('')) {
         result.textContent = '홈런';
@@ -56,11 +67,11 @@ const asyncFunc = event => {
         }
         wrong++;
         if (wrong > 3) {
-            result.textContent = `u fucked up. Game reset. The Answer was ${numArr.join('')}. Guess new Number.`;
+            result.textContent = `Game over. Game reset. The Answer was ${numArr.join('')}. Guess new number.`;
             generateNum();
             wrong = 0;
         } else {
-            result.textContent = `${strike}strike, ${ball}ball, You got ${4 - wrong} chance`;
+            result.textContent = `${strike}strike, ${ball}ball, You got ${4 - wrong} chance.`;
         }
     }
 }
---
+++ lotto/lotto.js
@@ -0,0 +1,7 @@
+let body = document.body;
+let numbers = [...Array(46).keys()]
+
+numbers.shift(0)
+
+var pickedNum = Math.floor(Math.random()*45) +1;
+
