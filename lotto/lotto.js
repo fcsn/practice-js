fa827219d6fe9404a0cc8d4453a6173e45f0d795
sirfcsn <sirfcsn@gmail.com>
2019-01-09 23:48:30 +0900

    feat: Add pick random num feat in lotto.js

--- lotto/lotto.js
+++ lotto/lotto.js
@@ -1,7 +1,21 @@
 let body = document.body;
 let numbers = [...Array(46).keys()]
 
+let num;
+
 numbers.shift(0)
 
-var pickedNum = Math.floor(Math.random()*45) +1;
+let pickedNum = Math.floor(Math.random()*45) +1;
 
+do {
+    num = pickedNum;
+    for (let j = 0; j <= i; j++) {
+        if (num === result[j]) break;
+    }
+    if (j < i) continue;
+    result[i] = num;
+    i++;
+} while (i < 6)
+for (let i = 0; i < 6; i++) {
+    console.log(result[i] + ',');
+}
